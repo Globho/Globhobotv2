2d71c166826129d04a3d57b2332d4aacadefa608
Globho <116518821+Globho@users.noreply.github.com>
2023-08-28 17:46:54 +0200

    Update gc-kick4.js.js

--- plugins/gc-kick4.js.js
+++ plugins/gc-kick4.js.js
@@ -1,9 +1,1 @@
-let handler = async (m, { conn, isAdmin }) => {
-  if (m.fromMe) throw 'Nggk'
-  if (isAdmin) throw '*ORA SEI AMMINISTRATORE 😀*'
-  await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
-}
-handler.command = /^qwerty$/i
-handler.g1 = true
-handler.botAdmin = true
-export default handler
+
